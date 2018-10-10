c2ae4a09a31f557c92b0c2915b02cf0a96770fb9
sarahgeissler <sarahgeissler@wifi-roaming-128-4-76-190.host.udel.edu>
2018-10-09 21:38:18 -0400

    Commit Quiz 3 Code

---
+++ Quiz3/src/pkgCore/Deck.java
@@ -0,0 +1,66 @@
+package pkgCore;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+import pkgEnum.eRank;
+import pkgEnum.eSuit;
+import pkgException.DeckException;
+
+public class Deck {
+
+	private ArrayList<Card> cardsInDeck = new ArrayList<Card>();
+
+	public Deck() {
+		for (eSuit eSuit : eSuit.values()) {
+			for (eRank eRank : eRank.values()) {
+				cardsInDeck.add(new Card(eSuit, eRank));
+			}
+		}
+		Collections.shuffle(cardsInDeck);
+	}
+
+	public Card Draw() throws DeckException {
+
+		if (cardsInDeck.size() == 0)
+		{
+			throw new DeckException(this);
+		}
+		return cardsInDeck.remove(0);
+
+	}
+
+	public Card Draw(eSuit eSuit) {
+		for (Card c : cardsInDeck) {
+			if (c.geteSuit() == eSuit) {
+				cardsInDeck.remove(c);
+				return (c);
+			}
+		}
+		return (null);
+	}
+
+	public int getiDeckCount()
+	{
+		return cardsInDeck.size();
+	}
+	
+	public int getRemaining(Object eNum){
+		int sum = 0;
+	    if(eNum instanceof pkgEnum.eSuit) {
+	        for (Card c: cardsInDeck) {
+	        	if (c.geteSuit() == eNum) {
+	        		sum++;
+	        	}
+	        }
+	    }
+	    else if (eNum instanceof pkgEnum.eRank) {
+	        for (Card c: cardsInDeck) {
+	        	if (c.geteRank() == eNum) {
+	        		sum++;
+	        	}
+	        }
+	        }
+	    return sum;
+	    }
+	}
